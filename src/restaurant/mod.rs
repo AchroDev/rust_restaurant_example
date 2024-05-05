769156f8c4aede3a05079d19cc594db79be978ce
AchroDev <ztblaine@protonmail.com>
2024-05-05 17:21:10 -0500

    Initial Commit

---
+++ src/restaurant/mod.rs
@@ -0,0 +1,10 @@
+mod pizza_order {
+    pub struct Pizza {
+        pub dough: String,
+        pub cheese: String,
+        pub topping: String,
+    }
+    impl Pizza {
+        todo!("TODO:");
+    }
+}
